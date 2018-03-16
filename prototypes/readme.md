0332cb372389c96a0a68464f5448191cc686bd3c
Jonathan Brandel <jono@with.in>
2018-03-16 13:56:02 -0700

    simplified Soundboard

--- prototypes/readme.md
+++ prototypes/readme.md
@@ -2,4 +2,5 @@
 
 + [Audio Test](./audio-test.html)
 + [Simple Audio Cue](./frame-player.html?file=./simple-audio-cue.json)
-+ [Clip Launcher](./frame-player.html?file=./soundboard.json)
++ ~~[Clip Launcher](./frame-player.html?file=./soundboard.json)~~
++ [Clip Launcher](./frame-player.html?file=/./webaudio-soundboard.json)
---
+++ release/third-party/WebAudio.js
@@ -0,0 +1,192 @@
+/**
+ * @author mrdoob / http://mrdoob.com/
+ */
+
+function WebAudio( context ) {
+
+	if ( context === undefined ) {
+
+		context = WebAudio.context;
+
+	}
+
+	var scope = this;
+	var src, source, buffer, binary;
+
+	var currentTime = 0;
+	var loop = false;
+	var playbackRate = 1;
+
+	var paused = true;
+	var startAt = 0;
+
+	var volume, onLoad, loaded = false;
+
+	if ( context ) {
+
+		createVolume();
+
+	}
+
+	function load( url ) {
+
+		var request = new XMLHttpRequest();
+		request.open( 'GET', url, true );
+		request.responseType = 'arraybuffer';
+		request.addEventListener( 'load', function ( event ) {
+			binary = event.target.response;
+			loaded = true;
+			if ( context ) {
+				decode();
+			}
+			if ( onLoad ) {
+				onLoad();
+			}
+		} );
+		request.send();
+
+	}
+
+	function decode() {
+
+		context.decodeAudioData( binary, function ( data ) {
+			buffer = data;
+			if ( paused === false ) play();
+		} );
+
+	}
+
+	function createVolume() {
+
+		if ( !context.volume ) {
+			context.volume = context.createGain();
+			context.volume.connect( context.destination );
+			context.volume.gain.value = 1;
+		}
+
+		volume = context.createGain();
+		volume.connect( context.volume );
+		volume.gain.value = 1;
+
+	}
+
+	function getCurrentTime() {
+
+		if ( buffer === undefined || paused === true ) return currentTime;
+		return currentTime + ( context.currentTime - startAt ) * playbackRate;
+
+	}
+
+	function play() {
+
+		if ( context === undefined ) {
+
+			context = WebAudio.getContext();
+			createVolume();
+
+			paused = false;
+			decode();
+
+		}
+
+		if ( buffer === undefined ) return;
+
+		source = context.createBufferSource();
+		source.buffer = buffer;
+		source.loop = loop;
+		source.playbackRate.value = playbackRate;
+		source.start( 0, currentTime );
+		source.connect( volume );
+
+		startAt = context.currentTime;
+
+	}
+
+	function stop() {
+
+		if ( buffer === undefined ) return;
+
+		source.stop();
+		source.disconnect( volume );
+
+		currentTime = getCurrentTime();
+
+	}
+
+	return {
+		play: function () {
+			if ( paused ) {
+				play(); paused = false;
+			}
+		},
+		pause: function () {
+			if ( paused === false ) {
+				stop(); paused = true;
+			}
+		},
+		get volume() {
+			return volume.gain.value;
+		},
+		set volume(v) {
+			volume.gain.value = v;
+		},
+		get currentTime() {
+			return getCurrentTime();
+		},
+		set currentTime( value ) {
+			if ( paused === false ) stop();
+			currentTime = value;
+			if ( paused === false ) play();
+		},
+		get playbackRate() {
+			return playbackRate;
+		},
+		set playbackRate( value ) {
+			if ( paused === false ) stop();
+			playbackRate = value;
+			if ( paused === false ) play();
+		},
+		get src() {
+			return src;
+		},
+		set src( url ) {
+			load( url );
+			src = url;
+		},
+		set onLoad( callback ) {
+			if ( loaded && callback ) {
+				callback();
+			} else if ( callback ) {
+				onLoad = callback;
+			}
+		},
+		get loop() {
+			return loop;
+		},
+		set loop( value ) {
+			loop = value;
+		},
+		get paused() {
+			return paused;
+		},
+		get buffer() {
+			return buffer;
+		},
+		get duration() {
+			return buffer ? buffer.duration : 0;
+		}
+	}
+
+}
+
+WebAudio.getContext = function() {
+
+	if ( WebAudio.context ) {
+		return WebAudio.context;
+	}
+
+	WebAudio.context = new ( window.AudioContext || window.webkitAudioContext )();
+
+	return WebAudio.context;
+
+};
