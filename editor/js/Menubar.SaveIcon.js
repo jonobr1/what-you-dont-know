a8c083e23573ee271dc67e2a2632988e8755c4eb
Jono <developer@jono.fyi>
2019-05-14 18:11:19 +0200

    Simplified Code Flow

--- editor/js/Menubar.SaveIcon.js
+++ editor/js/Menubar.SaveIcon.js
@@ -11,19 +11,6 @@ Menubar.SaveIcon = function ( editor ) {
   container.setId( 'save-icon' );
   container.setClass( 'menu' );
 
-  for ( var name in editor.signals ) {
-
-    var signal = editor.signals[ name ];
-    signal.add( mark );
-
-  }
-
-  function mark() {
-
-    container.addClass( Menubar.SaveIcon.DirtyMarker );
-
-  }
-
   return container;
 
 };
